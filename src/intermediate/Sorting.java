560e68b1a5fa8011963925a75d695e1ad9d354f9
ousmane <sangaryousmane@gmail.com>
2023-06-29 01:52:34 +0800

    bubble sort done

--- src/intermediate/Sorting.java
+++ src/intermediate/Sorting.java
@@ -1,4 +1,43 @@
 package intermediate;
 
+import java.util.Arrays;
+
 public class Sorting {
+
+    public static void bubbleSort(int[] arr) {
+
+        // Initialize whether the array is swapped
+        boolean isSwapped;
+
+        // run the steps until the end of the array (n-1)
+        for (int i = 0; i < arr.length; i++) {
+            isSwapped = false; // swapping hasn't occurred yet
+
+            // For each step, the max item will come at the last respective index
+            for (int j = 1; j < arr.length - i; j++) {
+
+
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    isSwapped = true; // After initial swap
+                }
+            }
+
+            // Break the loop and return the elements in the array if it's already sorted
+            // Array is already sorted when there is no swapping done.
+            if (!isSwapped) {
+                break;
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+
+        // check if the current item is smaller than the previous item
+
+        // Swap if the current item is smaller than the previous item
+
+
+    }
 }
