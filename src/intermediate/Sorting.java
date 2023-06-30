e5ae187d26f5fc952726705c071fa2b478ff9e4e
ousmane <sangaryousmane@gmail.com>
2023-06-30 19:02:18 +0800

    solve the missing element in an array using cyclic sort

--- src/intermediate/Sorting.java
+++ src/intermediate/Sorting.java
@@ -3,6 +3,37 @@ package intermediate;
 public class Sorting {
 
 
+
+    // Only number in the range that is missing from the array
+    // https://leetcode.com/problems/missing-number/description/
+    public static int missingNumber(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correctIndex = arr[i]; // index of current element
+
+            // Swap the elements of the array
+            if (arr[i] < arr.length && arr[i] != arr[correctIndex]){
+                int temp = arr[i];
+                arr[i] = arr[correctIndex];
+                arr[correctIndex] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+
+        // Check for the missing number in the array
+        // case 1
+        for (int index = 0; index < arr.length; index++){
+            if (arr[index] !=index){
+                return index;  // if the element is not equal to it current index, then the index is the missing
+            }
+        }
+
+        // return the length of the element if the missing element is the
+        // ith element that is not in the array. eg: [0, 1, 2, 3]. missing element is 4
+        return arr.length;
+    }
     // Perform cyclic sort
     public static void cyclicSort(int[] arr) {
         // Iterate over the array once
